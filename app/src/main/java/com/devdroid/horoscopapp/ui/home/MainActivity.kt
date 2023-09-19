70a48c759f6fd3a559fb84be3ecb81489d00386b
EliasMena <eliasmenapech@gmail.com>
2023-09-18 21:43:53 -0600

    Initial commit

---
+++ app/src/main/java/com/devdroid/horoscopapp/ui/home/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.devdroid.horoscopapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.devdroid.horoscopapp.R
+import com.devdroid.horoscopapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+
+    }
+    private fun initUi(){
+        iniNavigation()
+    }
+
+    private fun iniNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/devdroid/horoscopapp/ui/horoscope/HorosFragment.kt
@@ -0,0 +1,24 @@
+package com.devdroid.horoscopapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.devdroid.horoscopapp.R
+import com.devdroid.horoscopapp.databinding.FragmentHorosBinding
+
+class HorosFragment : Fragment() {
+
+    private var _binding: FragmentHorosBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHorosBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
